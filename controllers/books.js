f3286b85fa16ac4d695b45e313cab592a5979621
Pierre-Emmanuel Law-Hune <122879897+PierreL-H@users.noreply.github.com>
2023-07-03 08:33:55 +0200

    add bestrating, get one book, delete and update routes

--- controllers/books.js
+++ controllers/books.js
@@ -1,6 +1,7 @@
 const sharp = require('sharp')
 const path = require('path')
 const Book = require('../models/Book')
+const fs = require('fs')
 
 exports.createBook = async (req, res, next) => {
   console.log('body: ', req.body)
@@ -51,5 +52,104 @@ exports.getAllBooks = async (req, res, next) => {
   } catch (error) {
     res.status(400).json({ error })
   }
+}
+
+exports.getBook = async (req, res, next) => {
+  const { id } = req.params
+  try {
+    const book = await Book.findById(id)
+
+    if(!book){
+      return res.status(404).json({ error: "Book not found" })
+    }
+
+    return res.status(200).json(book)
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
+}
+
+exports.getBestRatings = async (req, res, next) => {
+  try {
+    const books = await (await Book.find().sort({'averageRating': 'desc'})).slice(0, 3)
+
+    res.status(200).json(books)
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+}
+
+exports.deleteBook = async (req, res, next) => {
+  const { id } = req.params
+  const userId = req.auth.userId
+
+  try {
+    const book = await Book.findById(id)
+    console.log(book.userId)
+    console.log(userId)
+    if (!book) {
+      return res.status(404).json({message: 'book not found'})
+    }
+    
+    if (book.userId != userId){
+      return res.status(401).json({message: 'unauthorized'})
+    }
+    
+    const filename = book.imageUrl.split('/images/')[1]
+    await Book.deleteOne({_id: id})
+
+    console.log('imageUrl: ', book.imageUrl)
+    console.log('filename: ', filename)
+    try {
+      fs.unlinkSync(`images/${filename}`)
+    } catch (error) {
+      console.error(`Failed to delete file: ${error}`);
+    }
+
+    return res.status(200).json({message: 'livre supprimé'})
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
+}
+
+exports.updateBook = async (req, res, next) => {
+  const { id } = req.params
+
+  const body = req.file ? JSON.parse(req.body.book) : req.body
+  
+  try {
+    const book = await Book.findById(id)
+    if(!book){
+      return res.status(404).json({message: 'book not found'})
+    }
+
+    const userId = req.auth.userId
+    if(book.userId != userId){
+      return res.status(401).json({message: 'unauthorized'})
+    }
+    
+    delete body.userId
+    if(req.file){
+      const { buffer, originalname } = req.file
+      const fileExt = path.extname(originalname)
+      const fileName = originalname.replace(fileExt, '')
+      
+      const now = new Date()
+      const currentYear = String(now.getFullYear())
+      const currentMonth = String(now.getMonth() + 1).padStart(2, '0')
+      const currentDay = String(now.getDate()).padStart(2, '0')
+      const timestamp = currentYear+currentMonth+currentDay+now.getTime().toString()
+      const ref = `${timestamp}-${fileName}.webp`
+      await sharp(buffer).webp({quality: 20}).toFile('images/' + ref)
+      body.imageUrl = `${req.protocol}://${req.get('host')}/images/${ref}`
+    }
+
+    await Book.updateOne({_id: id}, body)
+    res.status(200).json({message: 'livre modifié'})
+
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
+  console.log(body)
 
 }
--- routes/books.js
+++ routes/books.js
@@ -5,11 +5,11 @@ const upload = require('../middlewares/multer-config')
 const auth = require('../middlewares/auth')
 
 router.get('/', booksCtrl.getAllBooks)
-// router.get('/bestratings', booksCtrl.getBestRatings)
-router.post('/', auth, upload.single('image') ,booksCtrl.createBook)
-// router.get('/:id', booksCtrl.getBook)
-// router.put('/:id', booksCtrl.updateBook)
-// router.delete('/:id', booksCtrl.deleteBook)
+router.get('/bestrating', booksCtrl.getBestRatings)
+router.post('/', auth, upload.single('image'), booksCtrl.createBook)
+router.get('/:id', booksCtrl.getBook)
+router.put('/:id', auth, upload.single('image'),  booksCtrl.updateBook)
+router.delete('/:id', auth, booksCtrl.deleteBook)
 // router.post('/:id/ratings', booksCtrl.addRating)
 
 module.exports = router
